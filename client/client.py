ae36148d8ba82f976fb3dfb0bf098cdaac9e28d4
pavanmanishd <pavanmanishd@gmail.com>
2023-12-08 00:40:44 +0530

    Add client code for socket communication

---
+++ client/client.py
@@ -0,0 +1,61 @@
+import socket
+import threading
+import json
+
+SERVER_IP = '192.168.0.107'
+ID = 1
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def execute_task(data):
+    print(data.data)
+    # Upper case the data and return it to the server
+    result = data.data.upper()
+    message = json.dumps({
+        "client_id": ID,
+        "type": "result",
+        "data": result,
+        "task_id": data.task_id
+    })
+    server.send(message.encode('utf-8'))
+
+
+def recieve_message(server):
+    while True:
+        try:
+            data = server.recv(1024)
+            if not data:
+                break
+            data = json.loads(data.decode('utf-8'))
+            if data.type == "execute":
+                # print(data.data)
+                execute_task(data)
+            elif data.type == "result":
+                print(data.data)
+        except socket.error:
+            break
+
+def send_message(server):
+    while True:
+        input("Press Enter to send a message to the server... \n")
+        message = json.dumps({
+            "client_id": ID,
+            "type": "execute", # execute, result
+            "data": "Hello from client"
+        })
+        server.send(message.encode('utf-8'))
+
+
+def main():    
+    server.connect((SERVER_IP, 9999))
+    # print(socket.getaddrinfo(SERVER_IP, 9999))
+
+    recieve_thread = threading.Thread(target=recieve_message, args=(server,))
+    recieve_thread.start()
+
+    send_thread = threading.Thread(target=send_message, args=(server,))
+    send_thread.start()
+
+
+if __name__ == '__main__':
+    main()
