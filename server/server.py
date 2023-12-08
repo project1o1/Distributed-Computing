06284a490160b9a0253ddedc266b0bb042d842be
pavanmanishd <pavanmanishd@gmail.com>
2023-12-08 20:01:42 +0530

    Add task queue and lock for thread safety

--- server/server.py
+++ server/server.py
@@ -1,7 +1,7 @@
 import socket
 import queue
 import json
-from threading import Thread
+from threading import Thread, Lock
 
 class Server:
     def __init__(self, ip, port):
@@ -12,55 +12,60 @@ class Server:
 
         self.clients = []
         self.status = []
-        # self.tasks = queue.Queue()
-        # self.results = queue.Queue()
+        self.tasks = queue.Queue()
+        self.tasks_lock = Lock()
 
     def client_handler(self, client_socket, client_address):
-        tasks = queue.Queue()
-        results = queue.Queue()
-        length = 0
         def distribute_task(tasks):
             while True:
                 if tasks.empty():
                     continue
                 task = tasks.get()
                 words = task["data"].split()
-                for i, word in enumerate(words):
-                    length += 1
-                    data = json.dumps({
-                        "client_id" : task["client_id"],
-                        "task_id" : i,
-                        "type" : task["type"],
-                        "data" : word
-                    })
-                    for client_index, client in enumerate(self.clients):
-                        if self.status[client_index] == "idle":
-                            client.send(data.encode())
-                            self.status[client_index] = "busy"
-                            break
-                    
-        
+                with self.tasks_lock:
+                    for i, word in enumerate(words):
+                        data = json.dumps({
+                            "client_id": task["client_id"],
+                            "task_id": i,
+                            "type": task["type"],
+                            "data": word
+                        })
+                        for client_index, client in enumerate(self.clients):
+                            if self.status[client_index] == "idle":
+                                client.send(data.encode())
+                                self.status[client_index] = "busy"
+
         def gather_result(results):
             while True:
                 if results.empty():
                     continue
                 result = results.get()
+
+                # Check if 'task_id' key exists in the result dictionary
+                task_id = result.get("task_id", None)
+                if task_id is None:
+                    print("Received result without task_id:", result)
+                    continue
+                
                 data = json.dumps({
-                    "client_id" : result["client_id"],
-                    "task_id" : result["task_id"],
-                    "type" : result["type"],
-                    "data" : result["data"]
+                    "client_id": result["client_id"],
+                    "task_id": task_id,
+                    "type": result["type"],
+                    "data": result["data"]
                 })
+
                 for client_index, client in enumerate(self.clients):
                     if self.status[client_index] == "busy" and result["client_id"] == client_index:
                         client.send(data.encode())
                         self.status[client_index] = "idle"
                         break
-        
-        task_distribution_thread = Thread(target=distribute_task, args=(tasks,))
+
+
+
+        task_distribution_thread = Thread(target=distribute_task, args=(self.tasks,))
         task_distribution_thread.start()
 
-        gather_result_thread = Thread(target=gather_result, args=(results,))
+        gather_result_thread = Thread(target=gather_result, args=(self.tasks,))
         gather_result_thread.start()
 
         while True:
@@ -68,22 +73,50 @@ class Server:
             if not data:
                 continue
             print("Received data from " + str(client_address) + ": " + data.decode())
-            # tasks.put(json.loads(data.decode()))
             data = json.loads(data.decode())
             if data["type"] == "task":
-                tasks.put(data)
+                with self.tasks_lock:
+                    # Add the task to the queue
+                    self.tasks.put(data)
+                    # Update the queue size
+                    print("Task added to queue")
+                    print("Queue size:", self.tasks.qsize())
             elif data["type"] == "result":
-                results.put(data)
                 self.status[data["client_id"]] = "idle"
-    
+                print("Received result from client " + str(data["client_id"]) + ": " + data["data"])
+                #send it back to the original client
+                client_socket.send(data.encode())
+
     def accept_client(self):
         client_socket, client_address = self.socket.accept()
         print("Accepted connection from: " + str(client_address))
-        self.clients.append(client_socket)
-        self.status.append("idle")
 
-        client_thread = Thread(target=self.client_handler, args=(self, client_socket, client_address))
+        # Extract "client_id" from the incoming data
+        data = json.loads(client_socket.recv(1024).decode('utf-8'))
+        client_id = data["client_id"]
+
+        # Ensure the status list has enough elements for the client's client_id
+        while len(self.status) <= client_id:
+            self.status.append("")
+
+        self.clients.append(client_socket)
+        self.status[client_id] = "idle"
+
+        client_thread = Thread(target=self.client_handler, args=(client_socket, client_id))
         client_thread.start()
+
 
 if __name__ == "__main__":
     server = Server("0.0.0.0", 5000)
+
+    try:
+        server.socket.listen(5)
+
+        while True:
+            server.accept_client()
+
+    except KeyboardInterrupt:
+        print("Server shutting down.")
+
+    finally:
+        server.socket.close()
