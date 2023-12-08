19688f435e9c7984e0b48403aa6696c5a5b306b1
pavanmanishd <pavanmanishd@gmail.com>
2023-12-08 20:01:50 +0530

    Update server IP address and add delay before sending messages

--- client/client.py
+++ client/client.py
@@ -1,8 +1,9 @@
 import socket
 import threading
 import json
+import time
 
-SERVER_IP = '192.168.0.107'
+SERVER_IP = '127.0.0.1'
 ID = 1
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -13,56 +14,52 @@ def send_message(server, message):
         print(f"Failed to send message: {e}")
 
 def execute_task(data):
-    print(data.data)
-    # Upper case the data and return it to the server
-    result = data.data.upper()
+    print(data["data"])
+    result = data["data"].upper()
     message = json.dumps({
         "client_id": ID,
         "type": "result",
         "data": result,
-        "task_id": data.task_id
+        "task_id": data["task_id"]
     })
-    # server.send(message.encode('utf-8'))
     send_message(server, message)
 
 
-def recieve_message(server):
+def receive_message(server):
     while True:
         try:
             data = server.recv(1024)
             if not data:
                 break
             data = json.loads(data.decode('utf-8'))
-            if data.type == "task":
-                # print(data.data)
+            if data["type"] == "task":
                 execute_task(data)
-            elif data.type == "result":
-                print(data.data)
+            elif data["type"] == "result":
+                # print("Result" +data["data"])
+                print("Result from client " + str(data["client_id"]) + ": " + data["data"])
         except socket.error:
             break
 
-def send_message(server):
+def send_message_server(server):
     while True:
-        input("Press Enter to send a message to the server... \n")
+        # input("Press Enter to send a message to the server... \n")
+        # wait for 10ms
+        time.sleep(1)
+
         message = json.dumps({
             "client_id": ID,
-            "type": "task", # execute, result
+            "type": "task",
             "data": "Hello from client"
         })
-        # server.send(message.encode('utf-8'))
         send_message(server, message)
 
-
-def main():    
-    server.connect((SERVER_IP, 9999))
-    # print(socket.getaddrinfo(SERVER_IP, 9999))
-
-    recieve_thread = threading.Thread(target=recieve_message, args=(server,))
+def main():
+    server.connect((SERVER_IP, 5000))
+    recieve_thread = threading.Thread(target=receive_message, args=(server,))
     recieve_thread.start()
 
-    send_thread = threading.Thread(target=send_message, args=(server,))
+    send_thread = threading.Thread(target=send_message_server, args=(server,))
     send_thread.start()
-
 
 if __name__ == '__main__':
     main()
